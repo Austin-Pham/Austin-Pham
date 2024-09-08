d8c7ea896e474561f5c749d05574b0d6bd710d79
Austin-Pham <142257212+Austin-Pham@users.noreply.github.com>
2024-09-08 23:53:58 +0000

    changes

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -46,14 +46,16 @@ $(document).ready(function () {
   dataShapes.push(shape);
 
   // TODO 2: add a new property to all data shapes
-  for(var i = 0; i < dataShapes.length; i++) {
+  Object.goodBehavior;
+
+  for (var i = 0; i < dataShapes.length; i++) {
     var currentShape = dataShapes[i];
 
     if (currentShape.color === "red") {
       currentShape.goodBehavior = "bounce";
     }
     else if (currentShape.color === "blue") {
-      currentShape.goodBehavior = "blink"
+      currentShape.goodBehavior = "blink";
     }
     else {
       currentShape.goodBehavior = "spin";
