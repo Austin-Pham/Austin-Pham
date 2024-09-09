c8a1c548fad34c8e483a7200b627f273d3118c0c
Austin-Pham <142257212+Austin-Pham@users.noreply.github.com>
2024-09-09 00:09:58 +0000

    changes

--- asd-projects/data-shapes/index.js
+++ asd-projects/data-shapes/index.js
@@ -46,7 +46,7 @@ $(document).ready(function () {
   dataShapes.push(shape);
 
   // TODO 2: add a new property to all data shapes
-  Object.goodBehavior;
+  dataShapes.goodBehavior;
 
   for (var i = 0; i < dataShapes.length; i++) {
     var currentShape = dataShapes[i];
@@ -61,6 +61,7 @@ $(document).ready(function () {
       currentShape.goodBehavior = "spin";
     }
   }
+ 
   
   // TODO 3-a: add a function that handles the static display type
   
